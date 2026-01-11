e828f6765aa9600384173783720d40a2c47b3abe
Gleb <120745894+Mertozqq@users.noreply.github.com>
2026-01-11 19:22:12 +0300

    Revert "Review"

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState, ArticleStateType } from './constants/articleProps';
+import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,26 +13,19 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [settings, setSettings] = useState(defaultArticleState);
-	const submitAction = (data: ArticleStateType) => {
-		setSettings(data);
-	}
-	const resetAction = () => {
-		setSettings(defaultArticleState);
-	}
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': settings.fontFamilyOption.value,
-					'--font-size': settings.fontSizeOption.value,
-					'--font-color': settings.fontColor.value,
-					'--container-width': settings.contentWidth.value,
-					'--bg-color': settings.backgroundColor.value,
+					'--font-family': defaultArticleState.fontFamilyOption.value,
+					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-color': defaultArticleState.fontColor.value,
+					'--container-width': defaultArticleState.contentWidth.value,
+					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm settings={settings} submitButtonAction={submitAction} resetButtonAction={resetAction}/>
+			<ArticleParamsForm />
 			<Article />
 		</main>
 	);
